e83d9da623333c820e8d0f808468386bce5e769e
felpsalvs <profelipealves@outlook.com>
2025-04-22 20:49:44 -0300

    Wrap AppNavigator with NavigationContainer to enable navigation functionality in the app.

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from './src/utils/nativewindSetup';
 import AppNavigator from './src/navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 // Import global styles for web
 // A importação condicional é tratada durante o bundling
@@ -12,8 +13,10 @@ export default function App() {
   return (
     <ThemeProvider>
       <SafeAreaProvider>
-        <StatusBar style="auto" />
-        <AppNavigator />
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <AppNavigator />
+        </NavigationContainer>
       </SafeAreaProvider>
     </ThemeProvider>
   );
